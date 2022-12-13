bd975aeb67ad084fecb473f301f912b651ad71d1
laumo15 <118842475+laumo15@users.noreply.github.com>
2022-12-13 15:31:25 +0100

    Added My Timezone option, polished styling

--- javascript/index.js
+++ javascript/index.js
@@ -10,14 +10,25 @@ function updateTime() {
           "h:mm:ss [<small>]A[</small]"
         )
     }
-    let parisElement = document.querySelector("#paris")
-    if (parisElement) {
-      let parisDateElement = parisElement.querySelector(".date")
-      let parisTimeElement = parisElement.querySelector(".time")
-      let parisTime = moment().tz("Europe/Paris")
+    let romeElement = document.querySelector("#rome")
+    if (romeElement) {
+      let romeDateElement = romeElement.querySelector(".date")
+      let romeTimeElement = romeElement.querySelector(".time")
+      let romeTime = moment().tz("Europe/Rome")
 
-      parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY")
-      parisTimeElement.innerHTML = parisTime.format(
+      romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY")
+      romeTimeElement.innerHTML = romeTime.format(
+        "h:mm:ss [<small>]A[</small>]"
+      )
+    }
+    let tokyoElement = document.querySelector("#tokyo")
+    if (tokyoElement) {
+      let tokyoDateElement = tokyoElement.querySelector(".date")
+      let tokyoTimeElement = tokyoElement.querySelector(".time")
+      let tokyoTime = moment().tz("Asia/Tokyo")
+
+      tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY")
+      tokyoTimeElement.innerHTML = tokyoTime.format(
         "h:mm:ss [<small>]A[</small>]"
       )
     }
@@ -40,10 +51,26 @@ function updateCity(event) {
         <div class="time">
           ${cityTime.format("h:mm:ss [<small>]A[</small>]")}
         </div>
-      </div> `
+      </div>
+      <div class="back"> Back to <a href="/">all cities</a></div>`
+    let body = document.querySelector("body");
+    let cityAmPm = cityTime.format("A").toLowerCase()
+    body.className = ""
+    body.classList.add(cityAmPm)
+}
+
+function setBackground() {
+    let body = document.querySelector("body")
+    let losAngelesTime = moment().tz("America/Los_Angeles")
+    let losAngelesAmPm = losAngelesTime.format("A").toLowerCase()
+    body.className = ""
+    body.classList.add(losAngelesAmPm)
 }
 
 updateTime();
+
+setBackground();
+
 setInterval(updateTime, 1000);
 
 let citySelectElement = document.querySelector("#city");
